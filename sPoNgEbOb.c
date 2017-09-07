6bbfaa7437216fd397b358aa439c223ff129a472
Caleb-Shepard <Caleb-Shepard@users.noreply.github.com>
2017-09-07 19:11:23 -0400

    Add files via upload
    personal tool added

---
+++ sPoNgEbOb.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void stupify(char *str)
+{
+    int modulate;
+
+    modulate = 1;
+    while(*str)
+    {
+        // if it's a letter, alternate
+        if(*str > 64 && *str < 123)
+        {
+            // logical optimization, still checking
+            if(*str < 91 || *str > 96)
+            {
+                modulate %= 2;
+
+                if(!modulate && *str > 96)
+                    printf("%c", *str - 32);
+                else if(modulate && *str < 91)
+                    printf("%c", *str + 32);
+                else
+                    printf("%c", *str);
+
+                modulate++;
+            }
+        }
+        else
+            printf("%c", *str);
+
+        str++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if(argc > 1)
+        stupify(argv[1]);
+
+    return 0;
+}
