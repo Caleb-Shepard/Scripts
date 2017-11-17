6ed8a5387d2352567a1ebf7d2ca2339ee11ce798
Tiger Sachse <tgsachse@gmail.com>
2017-11-17 11:03:44 -0500

    script to install sponge to /bin

---
+++ install_sponge.sh
@@ -0,0 +1,20 @@
+# Script to install sPoNgEbOb.c
+# Written by Tiger Sachse
+# sPoNgEbOb.c by Caleb Shepard
+
+# Change this variable to change the name of the folder created during install
+DOWNLOAD_FOLDER="temp-download-spongebob"
+
+git clone https://github.com/Caleb-Shepard/Scripts.git $DOWNLOAD_FOLDER
+cd $DOWNLOAD_FOLDER
+sudo gcc sPoNgEbOb.c -o /bin/sponge
+cd ..
+sudo rm -r $DOWNLOAD_FOLDER
+
+# Dorky stuff to print at the end :P
+echo
+echo
+echo \#########################################################
+echo Installation Successful! Try this command to get started:
+echo \$ sponge \"who lives in a pineapple under the sea?\"
+echo
