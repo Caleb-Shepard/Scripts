7ceb6a2ca07b6bccdc75e77d37bd2da03a9cc9b5
Caleb Shepard <cshepard6055@floridapoly.edu>
2017-12-04 08:19:15 -0500

    Add wifi_mac_anchor.sh

---
+++ wifi_mac_anchor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# with default os settings, run as root
+touch test.txt
+echo "[device]" >> /etc/NetworkManager/NetworkManager.conf
+echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
