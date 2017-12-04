60965f368f833445002c6acf63cd2c1462602efd
Caleb Shepard <cshepard6055@floridapoly.edu>
2017-12-04 08:22:54 -0500

    Update wifi_mac_anchor

--- wifi_mac_anchor.sh
+++ wifi_mac_anchor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # with default os settings, run as root
-touch test.txt
-echo "[device]" >> /etc/NetworkManager/NetworkManager.conf
-echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
+touch /etc/NetworkManager/conf.d/mac_anchor.conf
+echo "[device]" >> /etc/NetworkManager/conf.d/mac_anchor.conf
+echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/conf.d/mac_anchor.conf
 service network-manager restart
