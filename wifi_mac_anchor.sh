e83fbdcea62a3c1dd30206d067ee7a6ba87faffb
Caleb Shepard <cshepard6055@floridapoly.edu>
2017-12-04 08:20:33 -0500

    Update wifi_mac_anchor

--- wifi_mac_anchor.sh
+++ wifi_mac_anchor.sh
@@ -3,3 +3,4 @@
 touch test.txt
 echo "[device]" >> /etc/NetworkManager/NetworkManager.conf
 echo "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
+service network-manager restart
