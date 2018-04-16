a198cf92105dbf9ca8e52884c33f62f3265e71f5
Caleb Shepard <cshepard6055@floridapoly.edu>
2018-04-16 13:27:24 -0400

    Add vscode_key_repeat.sh

---
+++ Scripts_Mac_OS/vscode_key_repeat.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# marketplace instruction, here for convenience
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
