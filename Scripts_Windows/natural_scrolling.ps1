c5bef85557c4b77495975c4b133f972ff37a2d6a
Caleb Shepard <10443116+Caleb-Shepard@users.noreply.github.com>
2021-11-26 14:08:39 -0500

    Delete natural_scrolling.ps1

--- Scripts_Windows/natural_scrolling.ps1
+++
@@ -1,7 +0,0 @@
-# Modified from Jason Go's response
-# https://answers.microsoft.com/en-us/windows/forum/all/set-the-mouse-scroll-direction-to-reverse-natural/ede4ccc4-3846-4184-a86d-a028515040c0
-
-$mode = Read-host "Set the scroll direction - Default [0] | Natural [1]: "
-Get-PnpDevice -Class Mouse -PresentOnly -Status OK | ForEach-Object { "$($_.Name): $($_.DeviceID)"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters" -Name FlipFlopWheel -Value $mode
-"+--- Value of FlipFlopWheel is set to " + (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters").FlipFlopWheel + "`n" }
